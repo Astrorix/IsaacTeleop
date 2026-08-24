ade37f9d36a778f9771ccea9e132fa5e39bebf28
Andrei Aristarkhov <aaristarkhov@nvidia.com>
2026-08-24 10:05:06 -0700

    Publish serialized FlatBuffers from the tracker API (#865)
    Stripped T-types from python completely and most of C++ surface. The new Serialized<T> acts like a smart pointer to hold the backing storage to safely carry immutable flatbuffers views around.
    
    Breaking: removed *Tracked wrappers as redundant.
    
    Signed-off-by: Andrei Aristarkhov <aaristarkhov@nvidia.com>

--- tests/cpp/core/schema/test_full_body.cpp
+++ tests/cpp/core/schema/test_full_body.cpp
@@ -64,8 +64,6 @@ static_assert(sizeof(core::BodyJoints) == 24 * sizeof(core::BodyJointPose),
 // Type aliases resolve to the renamed generated types.
 static_assert(std::is_same_v<core::FullBodyPosePico, core::FullBodyPose>);
 static_assert(std::is_same_v<core::FullBodyPosePicoT, core::FullBodyPoseT>);
-static_assert(std::is_same_v<core::FullBodyPosePicoTracked, core::FullBodyPoseTracked>);
-static_assert(std::is_same_v<core::FullBodyPosePicoTrackedT, core::FullBodyPoseTrackedT>);
 static_assert(std::is_same_v<core::FullBodyPosePicoRecord, core::FullBodyPoseRecord>);
 static_assert(std::is_same_v<core::FullBodyPosePicoRecordT, core::FullBodyPoseRecordT>);
 static_assert(std::is_same_v<core::BodyJointsPico, core::BodyJoints>);
